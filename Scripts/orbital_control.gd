e68dedfe829ac615ad12d3dc7caa288b9d35fbc6
stur86 <simonesturniolo@gmail.com>
2023-02-12 16:09:50 +0000

    Controls linked

--- Scripts/orbital_control.gd
+++ Scripts/orbital_control.gd
@@ -1,3 +1,34 @@
 extends Node
 
 var orbital: Orbital
+
+signal new_quantum_numbers
+
+func set_n(n: int):
+	var l = orbital.l
+	var m = orbital.m
+	
+	set_quantum_numbers(n, l, m)
+	
+func set_l(l: int):
+	var n = orbital.n
+	var m = orbital.m
+	
+	set_quantum_numbers(n, l, m)
+	
+func set_m(m: int):
+	var n = orbital.n
+	var l = orbital.l
+	
+	set_quantum_numbers(n, l, m)
+	
+func set_quantum_numbers(n: int, l: int, m: int):
+	n = max(1, n)
+	l = min(l, n-1)
+	m = signi(m)*min(abs(m), l)
+	
+	orbital.n = n
+	orbital.l = l
+	orbital.m = m
+	
+	new_quantum_numbers.emit(n, l, m)
